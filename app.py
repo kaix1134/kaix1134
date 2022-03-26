d8770e5f1d018a54252b4984c8744ecf69d52aea
kaix1134 <102227220+kaix1134@users.noreply.github.com>
2022-03-26 15:53:25 +0100

    editing code

--- app.py
+++ app.py
@@ -1,32 +1,28 @@
-# ***************************************
-# Imports
-# ***************************************
-# Dash
 import dash
 from dash import html
 from dash import dcc
 from dash.dependencies import Input, Output
-
-# Div.
 import pandas as pd
 import numpy as np
 import calendar
-
-# Plotly
 import plotly.express as px
 import plotly.graph_objects as go
 
-# ***************************************
-# Get data
-# ***************************************
 import datamodel
+
+# ***************************************
+# getting data from datamodel.py
+# ***************************************
+
 order = datamodel.get_data()
+order2 = datamodel.get_data()
 df_year = datamodel.get_year()
 df_month = datamodel.get_month()
 
 # ***************************************
-# Diagram - Employee Sales
+# create figure 1 - sales by employee
 # ***************************************
+
 fig_employee = px.bar(order, 
     x='emp_name', y='total', 
     color='type', text='total', title='Sales by Employee',
@@ -36,9 +32,20 @@ fig_employee.update_traces(texttemplate='%{text:.2s}', textposition='outside')
 fig_employee.update_layout(uniformtext_minsize=8, uniformtext_mode='hide', xaxis_tickangle=45)
 
 # ***************************************
+# create figure 2 - sales by product
+# ***************************************
+
+fig_product = px.bar(order2, 
+    x='productname', y='total', 
+    color='type', text='total', title='Sales by Product',
+    hover_data=[],
+    labels={'total':'Total sales', 'productname':'Product', 'type':'Product Type'})
+fig_product.update_traces(texttemplate='%{text:.2s}', textposition='outside')
+fig_product.update_layout(uniformtext_minsize=8, uniformtext_mode='hide', xaxis_tickangle=45)
+
+# ***************************************
 # Activate the app
 # ***************************************
-#app = dash.Dash(__name__)
 
 dash_app = dash.Dash(__name__)
 app = dash_app.server
@@ -46,24 +53,27 @@ app = dash_app.server
 # ***************************************
 # Layout
 # ***************************************
+
 dash_app.layout = html.Div(
     children=[
         html.Div(className='row',
                 children=[
                     html.Div(className='four columns div-user-controls',
                             children=[
-                                html.H2('Sales dashboard'),
+                                html.H2('Employee Sales dashboard'),
                                 html.P('Select filters from dropdown'),
 
                     html.Div(children="Month", className="menu-title"),
                             dcc.Dropdown(
                                 id='drop_month',
-                                options=[{'label':selectmonth, 'value':selectmonth} for selectmonth in df_month['monthnames']],
+                                options=[{'label':selectmonth, 'value':selectmonth}
+                                         for selectmonth in df_month['monthnames']],
                             ),
                     html.Div(children="Year", className="menu-title"),
                             dcc.Dropdown(
                                 id='drop_year',
-                                options=[{'label':selectyear, 'value':selectyear} for selectyear in df_year]
+                                options=[{'label':selectyear, 'value':selectyear}
+                                         for selectyear in df_year]
                             ),
                             ]
                     ),
@@ -73,44 +83,100 @@ dash_app.layout = html.Div(
                             ]
                     ),
                 ]
+            ),
+
+        html.Div(className='row',
+                children=[
+                    html.Div(className='four columns div-user-controls',
+                            children=[
+                                html.H2('Product Sales dashboard'),
+                                html.P('Select filters from dropdown'),
+
+                    html.Div(children="Month", className="menu-title"),
+                            dcc.Dropdown(
+                                id='drop_month2',
+                                options=[{'label':selectmonth, 'value':selectmonth}
+                                         for selectmonth in df_month['monthnames']],
+                            ),
+                    html.Div(children="Year", className="menu-title"),
+                            dcc.Dropdown(
+                                id='drop_year2',
+                                options=[{'label':selectyear, 'value':selectyear}
+                                         for selectyear in df_year]
+                            ),
+                            ]
+                    ),
+                    html.Div(className='eight columns div-for-charts bg-grey',
+                            children=[
+                                dcc.Graph(id="sales_product", figure=fig_product)
+                            ]
+                    ),
+                ]
             )
         ]
 )
 
+
 # ***************************************
-# Callbacks
+# callbacks
 # ***************************************
-# Output er diagrammet
-# Input er DropDown
-@dash_app.callback(Output('sales_employee', 'figure'),
-              [Input('drop_month', 'value')],
-              [Input('drop_year', 'value')])
+
+@dash_app.callback(
+    Output('sales_employee', 'figure'),
+    [Input('drop_month', 'value'),
+    Input('drop_year', 'value')],
+)
 
 def update_graph(drop_month, drop_year):
     if drop_year:
         if drop_month:
-            # Data i både drop_month og drop_year
-            order_fig1 = order.loc[(order['orderyear'] == drop_year) & (order['ordermonth'] == drop_month)]
+            order_fig1 = order.loc[(order['orderyear'] == drop_year) &
+                                   (order['ordermonth'] == drop_month)]
         else:
-            # Data i drop_year. men ikke drop_month
             order_fig1 = order.loc[order['orderyear'] == drop_year]
     else:
         if drop_month:
-            # Data i drop_month, men ikke drop_year
             order_fig1 = order.loc[order['ordermonth'] == drop_month]
         else:
-            # Ingen data - ikke noget valgt
             order_fig1 = order
-        
+
     return {'data':[go.Bar(
-        x = order_fig1['productname'],
+        x = order_fig1['emp_name'],
         y = order_fig1['total']
             )
         ]
     }
 
+@dash_app.callback(
+    Output('sales_product', 'figure'),  
+    [Input('drop_month1', 'value'),
+    Input('drop_year1', 'value')]
+)
+def update_graph1(drop_month1, drop_year1):
+
+    if drop_year1:
+        if drop_month1:
+            order_fig2 = order2.loc[(order2['orderyear'] == drop_year1) & (order2['ordermonth'] == drop_month1)]
+            
+        else:
+            order_fig2 = order2.loc[order2['orderyear'] == drop_year1]
+            
+    else:
+        if drop_month1:
+            order_fig2 = order2.loc[order2['ordermonth'] == drop_month1]
+            
+        else:
+            order_fig2 = order2
+        
+    return {'data':[go.Bar(
+        x = order_fig2['productname'],
+        y = order_fig2['total']
+            )
+        ]
+    }
+
 # ***************************************
-# Run the app
+# initiating the app
 # ***************************************
 if __name__ == '__main__':
     dash_app.run_server(debug=True)
