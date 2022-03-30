865dc3178a6309125de2c395fa5869e6d1c6eac9
kaix1134 <102227220+kaix1134@users.noreply.github.com>
2022-03-30 21:32:26 +0200

    Semi final version
    Able to access Azure Database, get data from it, merge it into a separate table and display it via Dash app

--- datamodel.py
+++ datamodel.py
@@ -1,69 +1,138 @@
-# Imports
+import mysql.connector
+from mysql.connector import MySQLConnection, Error
+from mysql_dbconfig import read_db_config
 import pandas as pd
-import numpy as np
-import calendar
 
 # ***************************************
-# Import af datamodel
+# creating mysql connection
 # ***************************************
+def connect():
+     db_config = read_db_config()
+     conn = None
+     try:
+         print('Connecting to MySQL database...')
+         conn = MySQLConnection(**db_config)
 
-#githubpath = 'https://raw.githubusercontent.com/TueHellsternKea/dashdemo/main/data/'
-githubpath = './data/'
+         if conn.is_connected():
+             print('Connection established')
 
-# Import from Excel file, 4 different sheets
-df_customers = pd.read_excel(githubpath + "my_shop_data.xlsx", sheet_name="customers")
-df_order = pd.read_excel(githubpath + "my_shop_data.xlsx", sheet_name="order")
-df_employee = pd.read_excel(githubpath + "my_shop_data.xlsx", sheet_name="employee")
-df_products = pd.read_excel(githubpath + "my_shop_data.xlsx", sheet_name="products")
+             #get data from database
 
+             data = get_data(conn)
+             return data
 
-def get_data():
-    # Employee name
-    df_employee['emp_name'] = df_employee['firstname'] + ' ' + df_employee['lastname']
+         else:
+             print('Connection failed')
 
-    # Customers name
-    df_customers['cust_name'] = df_customers['first_name'] + ' ' + df_customers['last_name']
+     except Error as error:
+         print(error)
 
-    # Data - Add: total, order, year, month
-    df_order['total'] = df_order['unitprice'] * df_order['quantity']
-    df_order['deliverytime'] = df_order['deliverydate'] - df_order['orderdate']
-    df_order['orderyear'] = df_order['orderdate'].dt.strftime("%Y")
-    df_order['ordermonth'] = pd.to_datetime(df_order['orderdate'])
-    df_order['ordermonth'] = df_order['ordermonth'].dt.month_name()
+     finally:
+         disconnect(conn)
 
-    # ***************************************
-    # Data - Relationer
-    # ***************************************
-    order = pd.merge(df_order, df_products, on='product_id')
-    order = pd.merge(order, df_employee, on='employee_id')
+         if __name__ == '__main__':
+            connect()
+
+# ***************************************
+# close database connecting
+# ***************************************
+def disconnect(conn):
+    if conn is not None and conn.is_connected:
+        conn.close()
+
+# ***************************************
+# getting data from database
+# ***************************************
+def get_data(conn):
+    cursor = conn.cursor()
+
+    # creating product dataframe
+    cursor.execute('Select * from products')
+    products = list(cursor.fetchall())
+    product_df = pd.DataFrame(products, columns=['product_id', 'productname', 'stock', 'reorder', 'type'])
+
+    # creating order dataframe
+    cursor.execute('Select * from orders')
+    orders = list(cursor.fetchall())
+    order_df = pd.DataFrame(orders, columns=['order_id', 'product_id', 'unitprice',
+                                             'quantity', 'customer_id', 'employee_id'])
+
+    # creating employee dataframe
+    cursor.execute('Select * from employees')
+    employees = list(cursor.fetchall())
+    employee_df = pd.DataFrame(employees, columns=['employee_id', 'firstname', 'lastname', 'date_of_birth'])
+    employee_names = merge_employee_names(employee_df)
+
+    # creating customer dataframe
+    cursor.execute('Select * from customers')
+    customers = list(cursor.fetchall())
+    customer_df = pd.DataFrame(customers, columns=['customer_id', 'first_name', 'last_name'])
+    customer_names = merge_customer_names(customer_df)
+
+    customer_df.insert(len(customer_df.columns), 'cust_name', customer_names)
+    employee_df.insert(len(employee_df.columns), 'emp_name', employee_names)
+
+    # add new columns to the order dataframe order_df
+
+    total_price=[]
+
+    for i in range(0, len(order_df.get('order_id'))):
+        total_price.append(float(order_df.get('unitprice')[i])
+                                 * float(order_df.get('quantity')[i].item()))
+
+    order_df.insert(len(order_df.columns), 'total', total_price)
+
+    # merge tables
+
+    merged_table = pd.merge(order_df, product_df, on='product_id')
+    merged_table = pd.merge(merged_table, employee_df, on='employee_id')
+    merged_table = pd.merge(merged_table, customer_df, on='customer_id')
+
+    merged_table = merged_table[['order_id', 'product_id', 'productname', 'type', 'customer_id', 'cust_name',
+                                 'employee_id', 'emp_name', 'total']]
+
+    return merged_table
+
+# ***************************************
+# function for merging customer names
+# ***************************************
+def merge_customer_names(list):
+    customer_names = []
+    for i in range(0,len(list.get('first_name'))):
+        customer_names.append(str(list.get('first_name')[i] + ' ' +
+                                  list.get('last_name')[i]))
+
+    return customer_names
+
+# ***************************************
+# function for merging employee names
+# ***************************************
+def merge_employee_names(list):
+    employee_names = []
+    for i in range(0,len(list.get('firstname'))):
+        employee_names.append(str(list.get('firstname')[i] + ' ' +
+                                  list.get('lastname')[i]))
+
+    return employee_names
+
+# ***************************************
+# creating datamodel to import into app.py
+# ***************************************
+def  create_datamodel(df_products,df_orders,df_employees,df_customers):
+    df_employees['emp_name'] = df_employees['firstname'] + ' ' + df_employees['lastname']
+    df_customers['cust_name'] = df_customers('first_name') + ' ' + df_customers['last_name']
+    df_orders['total'] = df_orders['unitprice'] * df_orders['quantity']
+
+    #df_order['deliverytime'] = df_order['deliverydate'] - df_order['orderdate']
+    #df_order['orderyear'] = df_order['orderdate'].dt.strftime("%Y")
+    #df_order['ordermonth'] = pd.to_datetime(df_order['orderdate'])
+    #df_order['ordermonth'] = df_order['ordermonth'].dt.month_name()
+
+    order = pd.merge(df_orders, df_products, on='product_id')
+    order = pd.merge(order, df_employees, on='employee_id')
     order = pd.merge(order, df_customers, on='customer_id')
-
-    # Order - Select colomns
-    order = order[['order_id', 
+    order = order[['order_id',
                 'product_id', 'productname', 'type',
-                'customer_id', 'cust_name', 'city', 'country',
-                'employee_id', 'emp_name', 
-                'orderdate', 'deliverydate', 'deliverytime', 'orderyear', 'ordermonth',
-                'total']]
-
-    # Retuner til app.py
+                'customer_id', 'cust_name',
+                'employee_id', 'emp_name', 'total']]
     return order
-
-
-def get_year():
-    # Year - Create a dataframe with years usede in the order dataframe
-    df_year = df_order['orderdate'].dt.strftime("%Y").unique()
-    df_year.sort()
-
-    return df_year
-
-
-def get_month():
-        # Month - Create a dataframe with month names
-    months = []
-    for x in range(1, 13):
-        months.append(calendar.month_name[x])
-
-    df_month = pd.DataFrame(months, columns=["monthnames"])
-
-    return df_month
